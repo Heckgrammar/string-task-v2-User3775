7b0a96d51a3d1cff025d07e010b8655787f5b7cf
Sarthak Sarkar <22SSarkar@heckgrammar.co.uk>
2025-06-21 15:39:56 +0100

    sdgsg

--- ExtractingStringPartsTask/Program.cs
+++ ExtractingStringPartsTask/Program.cs
@@ -12,7 +12,7 @@
 
             // Concatenate the contents of city and landmark * don't forget to add a space
 
-            // output the first character of county
+            // output the first character of country
 
             // output a concatenation of the first and last characters of city
 
@@ -27,9 +27,17 @@
             // California has n vowels
 
 
-            
+
 
             // Show your evidence of the program running in the Readme file
+            Console.WriteLine("Length of state name: " + state.Length);
+            Console.WriteLine("Concatenation of city name and landmark: " + city + " " + landmark);
+            Console.WriteLine("First character of country name: " + country[0]);
+            Console.WriteLine("Concatenation of first and last characters of city: " + city[0] + city[city.Length - 1]);
+            Console.WriteLine("Substring of landmark starting at c: " + landmark.Substring(landmark.IndexOf('c')));
+            Console.WriteLine("Substring of country from first S to first A: " + country.Substring(country.IndexOf('S'), country.IndexOf('A') - country.IndexOf('S')));
+            Console.WriteLine("Position of letter 'f' in state: " + (state.IndexOf('f') + 1));
+           // Console.WriteLine("California has " + CountVowels(state) + " vowels");
         }
     }
 }
