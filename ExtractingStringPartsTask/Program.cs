a4f89b02106bb73d7d3eca2eb5fa21704bea5b67
Sarthak Sarkar <22SSarkar@heckgrammar.co.uk>
2025-06-22 11:13:56 +0100

    done

--- ExtractingStringPartsTask/Program.cs
+++ ExtractingStringPartsTask/Program.cs
@@ -30,14 +30,51 @@
 
 
             // Show your evidence of the program running in the Readme file
-            Console.WriteLine("Length of state name: " + state.Length);
-            Console.WriteLine("Concatenation of city name and landmark: " + city + " " + landmark);
-            Console.WriteLine("First character of country name: " + country[0]);
-            Console.WriteLine("Concatenation of first and last characters of city: " + city[0] + city[city.Length - 1]);
-            Console.WriteLine("Substring of landmark starting at c: " + landmark.Substring(landmark.IndexOf('c')));
-            Console.WriteLine("Substring of country from first S to first A: " + country.Substring(country.IndexOf('S'), country.IndexOf('A') - country.IndexOf('S')));
-            Console.WriteLine("Position of letter 'f' in state: " + (state.IndexOf('f') + 1));
-           // Console.WriteLine("California has " + CountVowels(state) + " vowels");
+            int countryVowels = 0;
+            int stateVowels = 0;
+            int cityVowels = 0;
+            int landmarkVowels = 0;
+            Console.WriteLine("length of state name is " + state.Length);
+            Console.WriteLine("concatenation of city name and landmark is " + city + " " + landmark);
+            Console.WriteLine("first character of country name is " + country[0]);
+            Console.WriteLine("concatenation of first and last characters of city is " + city[0] + city[city.Length - 1]);
+            Console.WriteLine("substring of landmark starting at c is " + landmark.Substring(landmark.IndexOf('c')));
+            Console.WriteLine("substring of country from first S to first A is " + country.Substring(country.IndexOf('S'), country.IndexOf('A') - (country.IndexOf('S') - 1)));
+            Console.WriteLine("position of letter 'f' in state is " + (state.IndexOf('f') + 1) + " out of " + state.Length);
+           
+            for(int i = 0; i < country.Length; i++)
+            {
+                if ("aeiouAEIOU".Contains(country[i]))
+                {
+                    countryVowels++;
+                }
+            }
+            for (int i = 0; i < state.Length; i++)
+            {
+                if ("aeiouAEIOU".Contains(state[i]))
+                {
+                    stateVowels++;
+                }
+            }
+            for (int i = 0; i < city.Length; i++)
+            {
+                if ("aeiouAEIOU".Contains(city[i]))
+                {
+                    cityVowels++;
+                }
+            }
+            for (int i = 0; i < landmark.Length; i++)
+            {
+                if ("aeiouAEIOU".Contains(landmark[i]))
+                {
+                    landmarkVowels++;
+                }
+            }
+
+            Console.WriteLine("United States of America has " + countryVowels + " vowels");
+            Console.WriteLine("California has " + stateVowels + " vowels");
+            Console.WriteLine("San Fransicso has " + cityVowels + " vowels");
+            Console.WriteLine("Alcatraz Island has " + landmarkVowels + " vowels");
         }
     }
 }
